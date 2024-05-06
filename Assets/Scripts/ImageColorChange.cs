08abcdc549f4d0cb361e7c17900e531eeaad2d8d
Kjeld-H <kjeld.heemskerk@student.nhlstenden.com>
2024-05-06 12:56:53 +0200

    added status change for gripper

---
+++ Assets/Scripts/ImageColorChange.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject controller;
+    public Image statusButton;
+    public Sprite statusButtonOrange , statusButtonGreen;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Controleer of er acties worden uitgevoerd in de WarehouseController
+        if (controller.GetComponent<WarehouseController>().isZMoving ||
+            controller.GetComponent<WarehouseController>().isYMoving ||
+            controller.GetComponent<WarehouseController>().isXMoving)
+        {
+            
+             statusButton.sprite = statusButtonOrange;
+        }
+        else
+        {
+            statusButton.sprite = statusButtonGreen;
+        }
+    }
+}
