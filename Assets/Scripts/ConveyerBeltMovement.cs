9af2807abbca5ee9209cfbfcf053192ba099d7e0
KevinSpa <kevin.spalink@student.nhlstenden.com>
2024-05-30 21:37:14 +0200

    Return container to original X position

--- Assets/Scripts/ConveyerBeltMovement.cs
+++ Assets/Scripts/ConveyerBeltMovement.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class ConveyorBeltMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 5f; 
+    public float pauseDuration = 3f; 
+    public float positionTolerance = 0.1f; 
+
     private Rigidbody rb;
     private bool isMoving = false;
+    private bool isReversing = false;
     private bool onBelt = false;
+    private float initialBeltPosition;
 
     void Start()
     {
@@ -17,8 +22,17 @@ public class ConveyorBeltMovement : MonoBehaviour
     {
         if (isMoving)
         {
-            Vector3 movement = new Vector3(speed * Time.deltaTime, 0, 0);
+            float direction = isReversing ? -1 : 1;
+            Vector3 movement = new Vector3(speed * direction * Time.deltaTime, 0, 0);
             rb.MovePosition(transform.position + movement);
+
+            // Check if the container is back at the original X position
+            if (isReversing && Mathf.Abs(transform.position.x - initialBeltPosition) <= positionTolerance)
+            {
+                isMoving = false;
+                isReversing = false;
+                onBelt = false;
+            }
         }
     }
 
@@ -29,13 +43,24 @@ public class ConveyorBeltMovement : MonoBehaviour
             // Start moving when in contact with Belt
             onBelt = true;
             isMoving = true;
+            initialBeltPosition = transform.position.x;
         }
 
-        if (collision.gameObject.tag == "belt_back")
+        if (collision.gameObject.tag == "belt_back" && !isReversing)
         {
             // Stop moving when in contact with object that has belt_back tag
-            isMoving = false;
-            onBelt = false;
+            StartCoroutine(ReverseAfterPause());
+        }
+    }
+
+    private IEnumerator ReverseAfterPause()
+    {
+        isMoving = false;
+        yield return new WaitForSeconds(pauseDuration);
+        isReversing = true;
+        if (onBelt)
+        {
+            isMoving = true;
         }
     }
 }
