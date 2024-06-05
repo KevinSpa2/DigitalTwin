3ef47cbb5df30b09bd1287d6103769856b76beea
jariolyslager <jari.olijslager@student.nhlstenden.com>
2024-06-05 13:29:06 +0200

    Configuration button method + submodule inputfield generating

--- Assets/Scripts/GUI/Config/ConfigurationManager.cs
+++ Assets/Scripts/GUI/Config/ConfigurationManager.cs
@@ -10,19 +10,30 @@ using UnityEditor;
 public class ConfigurationManager : MonoBehaviour
 {
     [SerializeField]
-    private Transform modelParent, modelOptionsParent;
+    private Transform modelParent, modelOptionsParent, subModuleParent;
 
     [SerializeField]
     private List<GameObject> models;
 
+    [SerializeField]
+    private GameObject subModuleFieldPrefab;
+
     public void SelectModel(int index)
     {
         for(int i = 0; i < modelParent.transform.childCount; i++){
-            Destroy(modelParent.transform.GetChild(i).gameObject);     
+            Destroy(modelParent.transform.GetChild(i).gameObject);
+            // DOESNT DESTROY CONTAINERS
         }   
 
         GameObject selectedModel = Instantiate(models[index]);
         selectedModel.transform.SetParent(modelParent);
+
+        int yPosition = 625;
+
+        for(int j = 0; j < selectedModel.transform.childCount; j++){
+            GameObject subModule = Instantiate(subModuleFieldPrefab, new Vector3(1237, yPosition, 0), new Quaternion(0, 0, 0, 0), subModuleParent);
+            yPosition -= 85;
+        }
     }
 
 }
